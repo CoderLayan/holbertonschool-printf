7aa18fecb49d48a24a19f66c2f465947a0af5a71
layan <layanazez@gmail.com>
2025-03-23 17:47:04 +0000

    Implement _printf function with support for %c, %s, and %%

--- printf.c
+++ printf.c
@@ -2,9 +2,48 @@
 #include <stdarg.h>
 
 /**
+* handle_char - Handles the %c format specifier
+* @args: Argument list containing the character
+* Return: Number of characters printed
+*/
+int handle_char(va_list args)
+{
+char c = va_arg(args, int);
+return (_putchar(c));
+}
+
+/**
+* handle_string - Handles the %s format specifier
+* @args: Argument list containing the string
+* Return: Number of characters printed
+*/
+int handle_string(va_list args)
+{
+int count = 0;
+char *str = va_arg(args, char *);
+
+if (!str)
+str = "(null)";
+
+while (*str)
+count += _putchar(*str++);
+
+return (count);
+}
+
+/**
+* handle_percent - Handles the %% format specifier
+* Return: Number of characters printed
+*/
+int handle_percent(void)
+{
+return (_putchar('%'));
+}
+
+/**
 * _printf - Produces output according to a format
-* @format: Format string containing the characters and specifiers
-* Return: Number of characters printed or -1 on error
+* @format: Format string containing specifiers
+* Return: Number of characters printed
 */
 int _printf(const char *format, ...)
 {
@@ -20,12 +59,7 @@ while (*format)
 if (*format == '%')
 {
 format++;
-if (*format == '\0') /* Handle lone % */
-{
-va_end(args);
-return (-1); /* Error: Invalid format string */
-}
-else if (*format == 'c')
+if (*format == 'c')
 count += handle_char(args);
 else if (*format == 's')
 count += handle_string(args);
