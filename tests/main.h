5c08caf7e88bf2ac20c211652630b488ed8a7076
layan <layanazez@gmail.com>
2025-03-24 16:59:06 +0000

    Implement _printf function with support for %c, %s, and %%

--- tests/main.h
+++ tests/main.h
@@ -9,6 +9,7 @@ int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(void);
-int print_integer(va_list args); /* For %d and %i specifiers */
+int print_integer(va_list args);
+int print_number(int num); /* Helper for recursive digit printing */
 
 #endif /* MAIN_H */
